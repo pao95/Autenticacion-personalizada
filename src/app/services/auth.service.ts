13ef20dcbc8761be1b42d44363d247755855c319
Paola Hinostroza Trujillo <paola.hinostroza.trujillo.st@everis.com>
2020-03-09 11:57:12 -0300

    commit 9/3  11:57

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,14 @@ export class AuthService {
 
   private url = ' https://identitytoolkit.googleapis.com/v1/accounts:';
   private apiKey = 'AIzaSyAHPAZ8glbRHwpV3JYIND5nyKOQxEVVL94';
-
+userToken: string;
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+
+    this.leerToken();
+  }
 
 
   logout() {
@@ -30,6 +34,11 @@ export class AuthService {
     return this.http.post(
       `${this.url}signInWithPassword?key=${this.apiKey}`,
       authData
+      ).pipe(
+        map(  resp => {
+          this.guardarToken(resp['idToken']);
+          return resp;
+        })
       );
   }
 
@@ -43,6 +52,30 @@ export class AuthService {
     return this.http.post(
     `${this.url}signUp?key=${this.apiKey}`,
     authData
+    ).pipe(
+      map(  resp => {
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
+  }
+
+
+  private guardarToken(idToken: string) {
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+
+  }
+
+
+  leerToken() {
+    if (localStorage.getItem('token')) {
+        this.userToken = localStorage.getItem('token');
+    } else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
   }
 }
